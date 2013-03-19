99499a5d357b58792d9053cabb9826942fde7103
K <kalnajar09@gmail.com>
2013-03-19 12:44:24 -0400

    Initial Commit

---
+++ Camp Reservations App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Camp Reservations App
+//
+//  Created by Kinetic on 3/19/13.
+//  Copyright (c) 2013 Kinetic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
